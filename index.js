ff18905470af5a7d7e0efc2de060b2c7c6c80a60
Samuel <samsjara@gmail.com>
2024-02-02 17:55:00 -0300

    Just html and css

---
+++ index.js
@@ -0,0 +1,4 @@
+import {initialize} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase,ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+
